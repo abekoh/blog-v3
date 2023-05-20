dec93ca0c575bf4c8839ba38012779b9f4303898
Kotaro Abe <abekoh.bcky@gmail.com>
2023-05-20 21:47:50 +0900

    chore: lint:fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
-import {z, defineCollection} from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 const posts = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        summary: z.string().optional(),
-        categories: z.array(z.string()),
-        tags: z.array(z.string()),
-        publishedAt: z.date(),
-        modifiedAt: z.date().nullable(),
-        draft: z.boolean(),
-    }),
+  schema: z.object({
+    title: z.string(),
+    summary: z.string().optional(),
+    categories: z.array(z.string()),
+    tags: z.array(z.string()),
+    publishedAt: z.date(),
+    modifiedAt: z.date().nullable(),
+    draft: z.boolean(),
+  }),
 });
 export const collections = {
-    'posts': posts,
+  posts: posts,
 };
