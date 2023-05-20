ad52690d3af3561ff21299d1d025a0ee1cebac28
Kotaro Abe <abekoh.bcky@gmail.com>
2023-05-20 21:27:45 +0900

    fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,6 @@
 import {z, defineCollection} from 'astro:content';
 
 const posts = defineCollection({
-    type: 'content',
     schema: z.object({
         title: z.string(),
         summary: z.string().optional(),
