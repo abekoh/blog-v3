06128986e43b85e78d8429d99fd794e7788eb842
Kotaro Abe <abekoh.bcky@gmail.com>
2023-05-27 14:59:50 +0900

    feat: apply code style

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+        theme: "github-light",
+      wrap: true,
+    }
+  },
   integrations: [
     tailwind(),
     image({
