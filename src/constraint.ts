5489fc5f5e71f46b5e0b47f36ddfcbd07761edc6
Kotaro Abe <abekoh.bcky@gmail.com>
2023-05-28 17:57:14 +0900

    fix: meta

--- src/constraint.ts
+++ src/constraint.ts
@@ -1,6 +1,5 @@
 export const BLOG_TITLE = "abekoh's tech blog";
+export const BLOG_DESCRIPTION = "雑多になんでも技術メモ";
 export const PAGE_SIZE = 10;
-
 export const BASE_URL = "https://blog.abekoh.dev";
-
-export const AUTHOR_TWITTER = "@abekoh";
+export const AUTHOR_TWITTER = "@abekoh_bcky";
