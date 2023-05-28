39939d7920f92cc19f2306f009d64496b3eb52dc
Kotaro Abe <abekoh.bcky@gmail.com>
2023-05-29 00:17:21 +0900

    fix: rename

--- src/constraint.ts
+++ src/constraint.ts
@@ -1,4 +1,4 @@
-export const BLOG_TITLE = "abekoh's tech blog";
+export const BLOG_TITLE = "abekoh's tech note";
 export const BLOG_DESCRIPTION = "雑多になんでも技術メモ";
 export const PAGE_SIZE = 10;
 export const BASE_URL = "https://blog.abekoh.dev";
