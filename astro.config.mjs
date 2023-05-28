e9514c7bb1b624fea7f61052ad06f17bbb7a84d1
Kotaro Abe <abekoh.bcky@gmail.com>
2023-05-28 18:01:48 +0900

    fix: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: "github-dark",
-      wrap: false,
-    },
+      wrap: false
+    }
   },
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap()]
 });
