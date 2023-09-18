034b24607b854dd5a5c0dc9aaada1f3c1b8a5492
Kotaro Abe <abekoh.bcky@gmail.com>
2023-09-18 11:53:09 +0900

    add e2e tests (#43)

---
+++ playwright.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./e2e",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    baseURL: "http://localhost:4321",
+    trace: "on",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+  webServer: {
+    command: "pnpm run preview",
+    port: 4321,
+    timeout: 10 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+});
