352a97844d772755b4760351e79e0b8b04af75b4
Kotaro Abe <abekoh.bcky@gmail.com>
2023-09-18 11:49:57 +0900

    fix: lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,12 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  ignorePatterns: ["scripts/**/*", "dist/**/*", "node_modules/**/*", "playwright-report/**/*"],
+  ignorePatterns: [
+    "scripts/**/*",
+    "dist/**/*",
+    "node_modules/**/*",
+    "playwright-report/**/*",
+  ],
   root: true,
   overrides: [
     {
