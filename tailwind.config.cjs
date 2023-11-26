646635c194591f7716ab84f332fc5a3215d7a027
Kotaro Abe <abekoh.bcky@gmail.com>
2023-11-26 15:04:31 +0900

    fix font config (#97)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx]}"],
   theme: {
     fontFamily: {
-      sans: ["M PLUS 1p", "sans-serif"],
+      sans: ['"M PLUS 1p"', "sans-serif"],
     },
   },
   plugins: [],
