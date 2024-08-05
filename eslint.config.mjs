3b5c9e6fcd6c882ca738065d60355c5a290549cd
Kotaro Abe <abekoh.bcky@gmail.com>
2024-08-05 22:58:08 +0900

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,22 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintPluginAstro from 'eslint-plugin-astro';
-import globals from 'globals';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+import globals from "globals";
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...eslintPluginAstro.configs.recommended,
-    {
-        languageOptions: {
-            globals: {
-                ...globals.commonjs,
-            }
-        }
-    }
+  {
+    ignores: ["playwright-report/**/*", "dist/**/*", ".astro/**/*"],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.commonjs,
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+  },
 );
