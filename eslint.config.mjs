679f48a662137808a61fd6771bf38c2764bcce56
Kotaro Abe <abekoh.bcky@gmail.com>
2024-08-05 22:55:29 +0900

    lint:fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,17 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginAstro from 'eslint-plugin-astro';
+import globals from 'globals';
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
     ...eslintPluginAstro.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.commonjs,
+            }
+        }
+    }
 );
